5bb5b3b7e7fe077c9697a75a8a6cf0587898ea88
hemant12d <hsoni2962@gmail.com>
2023-03-13 18:12:32 +0530

    Exposure with of map

---
+++ hello.go
@@ -0,0 +1,66 @@
+// First program of go
+
+package main;
+
+// Second program of go
+func main() {
+
+    //Variables and types declarations
+    //var name string = "Hemant Soni";
+    //var age int = 18;
+    //var role string = "Backend Engineer";
+    //fmt.Println(name, age, role, job);
+
+
+    // Short variable declarations
+    //name := "Hemant Soni"
+    //age := 20;
+    //fmt.Println(name, age)
+
+
+
+    //With out type variable declarations
+    //var name = "heart";
+    //var age = 20;
+    //fmt.Println(name, age);
+
+
+
+    //Pointer has also back in golang
+    //var age int = 20;
+    //var ptr *int = &age;
+    //fmt.Println(*ptr);
+    //fmt.Println(ptr);
+
+
+
+    //Types in golang
+    //type intType int;
+    //var value intType = 18;
+    //var age intType = 20;
+    //fmt.Println(value, age)
+
+
+    // Loops
+
+    //For loop
+    //   var i int = 0;
+    //   for i < 10 {
+    //    fmt.Println(i);
+    //    i++;
+    //   }
+    //
+    //For loop with anther syntax
+    //   for j := 0; j < 10; j++ {
+    //    fmt.Println(j);
+    //   }
+
+    // For each loop
+
+    //var str string = "hemantsonion";
+    //
+    //for i, c := range str {
+    //   fmt.Printf("%d %c", i, c);
+    //   fmt.Println();
+    //}
+}
---
+++ map.go
@@ -0,0 +1,63 @@
+package main;
+
+import "fmt"
+
+func main() {
+
+	// Map declarations
+	var dev map[string]string;
+	fmt.Println(dev); // []
+
+    // Check map values
+    if dev == nil {
+        fmt.Println("dev map is nil");
+    } else {
+        fmt.Println("dev map is not nil")
+    }
+
+
+	// Get the length of the map
+	fmt.Println("Length", len(dev));
+
+	// Instantiate map object
+	var dev2  = map[string]string{};
+    fmt.Println("Instantiate map object", dev2);
+
+
+	// Map with predefined values and keys
+	var dev3 = map[string]string{
+		"name": "Hemant",
+		"age": "20",
+		"role": "Software Engineer",
+		"color": "green",
+	}
+	fmt.Println(dev3);
+
+
+	// Delete properties from the map
+	delete(dev3, "color");
+	fmt.Println(dev3);
+
+
+	// Add properties
+	dev3["color"] = "green";
+	fmt.Println(dev3);
+
+	// For each with mapping
+	for key, val := range dev3 {
+		fmt.Println(key, " : ", val);
+	}
+
+
+	// Check if properties exists or not in the map
+	name, ok := dev3["name"];
+	if ok {
+		fmt.Println("Name property is available", name);
+	}
+
+	salary, ok := dev3["salary"];
+	if !ok {
+        fmt.Println("Salary property is not available", salary);
+    }
+
+}
