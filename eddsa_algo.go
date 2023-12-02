87487deb4e671217907fbe38e0c3aa2b3df78219
Hemant Soni <hsoni2962@gmail.com>
2023-12-02 23:48:08 +0530

    main: Got the more understanding about sysmetric algo

--- eddsa_algo.go
+++ eddsa_algo.go
@@ -1,3 +1,31 @@
 package main
 
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"fmt"
+)
 
+func main(){
+	pubKey, priKey, err := ed25519.GenerateKey(rand.Reader)
+
+	fmt.Printf("pubKey: %v\n", priKey.Public().(ed25519.PublicKey))
+	fmt.Printf("priKey: %v\n", pubKey)
+	fmt.Printf("error: %v\n", err)
+}
+
+
+// Additional encryption information
+// Encryption/Decryption
+// SSL/PKI
+// Signature verification and signature generating
+// Key Exchange
+// SSL Authentication
+// File and Email encryption
+// Secure encryption transaction
+
+
+// Note
+// Encrypt and signature generation process is different not same
+// Hash, signature, encryption, signing, message is different and have own meaning
+// Public and Private key has different-2 uses based on scenario and use cases
