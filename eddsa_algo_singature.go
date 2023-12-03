bfe09175f27d8b505868ca4843d093ce0cd1c785
Hemant Soni <hsoni2962@gmail.com>
2023-12-04 00:40:04 +0530

    generate signature using eddsa algo, verify the signature, encode and decode the signature

--- eddsa_algo_singature.go
+++ eddsa_algo_singature.go
@@ -3,16 +3,43 @@ package main
 import (
 	"crypto/ed25519"
 	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 )
 
-func main(){
-	pubKey, priKey, err := ed25519.GenerateKey(rand.Reader)
+var message = "abc"
 
+func main(){
+	// Key generation
+	pubKey, priKey, err := ed25519.GenerateKey(rand.Reader)
 	fmt.Printf("pubKey: %v\n", priKey.Public().(ed25519.PublicKey))
 	fmt.Printf("priKey: %v\n", pubKey)
 	fmt.Printf("error: %v\n", err)
+
+	// Sign the message using the private key
+	signature := ed25519.Sign(priKey, []byte(message))
+	strSignature := base64.StdEncoding.EncodeToString(signature)
+	fmt.Printf("signature: %s\n", strSignature)
+
+
+	// Verifying the signature using public key
+	decodeSignature, err2 := base64.StdEncoding.DecodeString(strSignature)
+	if err2 != nil{
+		fmt.Printf("Error during decoding signature: %v", err2)
+	}
+
+	// Verify the signature using the public key
+	valid := ed25519.Verify(pubKey, []byte(message), decodeSignature)
+
+	if valid {
+		fmt.Println("Signature is valid.")
+	} else {
+		fmt.Println("Signature is not valid.")
+	}
+
 }
+
+
 
 
 // Additional encryption information
