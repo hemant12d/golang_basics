865322878d43046fba359b46d43272bfef5339b6
Hemant Soni <hsoni2962@gmail.com>
2023-12-05 23:38:41 +0530

    update eddsa algorithem code

--- eddsa_algo_singature.go
+++ eddsa_algo_singature.go
@@ -12,8 +12,14 @@ var message = "abc"
 func main(){
 	// Key generation
 	pubKey, priKey, err := ed25519.GenerateKey(rand.Reader)
-	fmt.Printf("pubKey: %v\n", priKey.Public().(ed25519.PublicKey))
-	fmt.Printf("priKey: %v\n", pubKey)
+	fmt.Printf("pubKey: %v\n", pubKey)
+
+	fmt.Printf("public key as string: %s\n", base64.StdEncoding.EncodeToString(pubKey))
+
+	pubKeyDecode, _ := base64.StdEncoding.DecodeString(base64.StdEncoding.EncodeToString(pubKey))
+	fmt.Printf("pubKey decoded: %v\n", pubKeyDecode)
+
+	fmt.Printf("priKey: %v\n", base64.StdEncoding.EncodeToString(priKey))
 	fmt.Printf("error: %v\n", err)
 
 	// Sign the message using the private key
@@ -21,11 +27,10 @@ func main(){
 	strSignature := base64.StdEncoding.EncodeToString(signature)
 	fmt.Printf("signature: %s\n", strSignature)
 
-
 	// Verifying the signature using public key
 	decodeSignature, err2 := base64.StdEncoding.DecodeString(strSignature)
 	if err2 != nil{
-		fmt.Printf("Error during decoding signature: %v", err2)
+		fmt.Printf("error during decoding signature: %v", err2)
 	}
 
 	// Verify the signature using the public key
