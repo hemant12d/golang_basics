aeb33f90ec713cb1087ebee6a5732f8f41c2b3b3
Hemant Soni <hsoni2962@gmail.com>
2023-12-10 23:42:17 +0530

    Remove additional log file

--- encryption_urlEncoding_standard.go
+++ encryption_urlEncoding_standard.go
@@ -10,11 +10,9 @@ func main() {
 	// Here's the `string` we'll encode/decode.
 	data := "abc123!?$*&()'-=@~"
 
-
-	// This encodes/decodes using a URL-compatible base64
-	// format.
 	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
 	fmt.Println(uEnc)
+
 	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
 	fmt.Println(string(uDec))
 }
