460985f5d37a2992e65fd22233bf2dfb6c9157fe
Hemant Soni <hsoni2962@gmail.com>
2023-12-22 00:52:35 +0530

    Add basic code golang strategy pattern

---
+++ authentication_strategy_pattern.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type AuthenticatorStrategy interface {
+	Authenticate(ctx context.Context, token []byte) error
+}
+
+type Authn struct {
+	strategy AuthenticatorStrategy
+}
+
+func main (){
+	fmt.Println("Hello from the world !")
+}
+
+type JWTStrategy struct {}
+type ApiStrategy struct {}
+type shareKeyStrategy struct{}
+
+func (svc *JWTStrategy) Authenticate(ctx context.Context, token []byte) (error) {
+	return nil
+}
+
+func (svc *ApiStrategy) Authenticate(ctx context.Context, token []byte) (error) {
+	return nil
+}
+
+func (svc *shareKeyStrategy) Authenticate(ctx context.Context, token []byte) (error) {
+	return nil
+}
