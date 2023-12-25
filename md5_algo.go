fe096a66338aff5d7925116e63564eef23b13d90
Hemant Soni <hsoni2962@gmail.com>
2023-12-25 23:41:45 +0530

    fix md_5 algo variable names

--- md5_algo.go
+++ md5_algo.go
@@ -11,14 +11,14 @@ func main() {
 	data1 := []byte("data 1 is here")
 	data2 := []byte("data 1 is here")
 
-	data1_hash := md5.Sum(data1)
-	data2_hash := md5.Sum(data2)
+	data1Hash := md5.Sum(data1)
+	data2Hash := md5.Sum(data2)
 
-	fmt.Printf("%x\n", data1_hash)
-	fmt.Printf("%x\n", data2_hash)
+	fmt.Printf("%x\n", data1Hash)
+	fmt.Printf("%x\n", data2Hash)
 
 	// Compare the two hash values
-	if bytes.Equal(data1_hash[:], data2_hash[:]) {
+	if bytes.Equal(data1Hash[:], data2Hash[:]) {
 		fmt.Println("The hash values are the same.")
 	} else {
 		fmt.Println("The hash values are different.")
